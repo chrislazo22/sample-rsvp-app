c1d5ffd3ffcb00dcca0b6fa5dc50ad5d72f21673
Christian Lazo <lazochristian.cl@gmail.com>
2019-03-17 15:12:32 -0700

    Add class to modify responded

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-const input = $('#registrar-input');
+var rsvp = rsvp || {};
 
-$('#registrar').submit(function (e) {
-  e.preventDefault();
-  const text = input.val();
-  const ul = $('#invitedList');
-  const li = $('<li></li>');
-  li.text(text);
-  ul.append(li);
-  input.val('');
+$(document).ready(function() {
+  rsvp.toggleResponded();
+  rsvp.addPerson();
 });
+
+rsvp.addPerson = function() {
+const input = $('#registrar-input');
+const ul = $('#invitedList');
+  $('#registrar').submit(function (e) {
+    e.preventDefault();
+    const text = input.val();
+    const li = $('<li></li>');
+    li.text(text);
+
+    const label = $('<label>');
+    label.text('Confirmed');
+    const checkbox = $('<input type="checkbox"/>');
+    label.append(checkbox);
+    li.append(label);
+    ul.append(li);
+    input.val('');
+  });
+
+}
+
+rsvp.toggleResponded = function() {
+const ul = $('#invitedList');
+  ul.on('change', function (e) {
+    const checkbox = e.target;
+    const checked = checkbox.checked;
+    const listItem = checkbox.parentNode.parentNode;
+
+    if (checked) {
+      listItem.className = 'responded';
+    } else {
+      listItem.className = '';
+    };
+  });
+}
